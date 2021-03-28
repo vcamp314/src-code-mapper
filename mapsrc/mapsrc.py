c6afa481bf512ee6f1487f8e9b7c58d475e7a8c2
V.C <seiri17725@gmail.com>
2021-03-28 21:23:05 +0900

    added filtering by file extension to directory scanning functionality

--- mapsrc/mapsrc.py
+++ mapsrc/mapsrc.py
@@ -5,15 +5,55 @@ The module provides the below function to run the mapping of src code at a given
 
 """
 
+import os
 
-def run_mapping(path):
+
+def run_mapping(path: str):
     """run mapping of src code at a given path location
-    >>> run_mapping('sample text')
+    >>> run_mapping('./path/to/src')
     sample text
 
     :param path:
     :return: None
     """
-    print(path)
+    config = {
+        'extensions': ['.js', '.jsx', '.ts', '.tsx', '.py', '.txt', '.rst']
+    }
+    print(scan(path, config['extensions']))
 
 
+def scan(path: str, extensions: list):
+    """run scan of src files of a given list of extensions at a given path
+    >>> scan('./path/to/src')
+    sample text
+
+    :param extensions: list of file extensions to be scanned
+    :param path: path of src code
+    :return: Dict - dictionary of results
+    """
+
+    # get list of file paths, filtered for given extensions, or unfiltered if no
+    # extensions are given:
+    file_list = [
+        os.path.join(r, f)
+        for r, _, fs in os.walk(path)
+        for f in fs
+        if any(f.endswith(x) for x in extensions) or not extensions
+    ]
+
+    return {'file_list': file_list}
+
+
+
+#############################################################
+
+
+def draw(src_map: str):
+    """run mapping of src code at a given path location
+    >>> draw('./path/to/src')
+    sample text
+
+    :param src_map:
+    :return: None
+    """
+    print(src_map)
